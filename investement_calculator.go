973a03c23ea05bb2faff78abee14b1f2abef89fe
Muhammad Talib Waseem <talib.waseem@securiti.ai>
2024-04-26 01:10:42 +0500

    Update investement_calculator.go

--- investement_calculator.go
+++ investement_calculator.go
@@ -6,10 +6,16 @@ import (
 )
 
 func main() {
-	var investemtAmount = 1000
+	const inflationRate float64 = 2.5
+	var investemtAmount float64 = 1000
 	var expectedReturnRate = 5.5
-	var years = 10
+	var years float64 = 10
 
-	var futureValue = float64(investemtAmount) * math.Pow(1+expectedReturnRate/100, float64(years))
+	// investemtAmount, years, expectedReturnRate := 1000.0, 10.0, 5.5 more consice
+
+	futureValue := investemtAmount * math.Pow(1+expectedReturnRate/100, years)
+	futureRealValue := futureValue / math.Pow(1+inflationRate/100, years)
+
 	fmt.Println(futureValue)
+	fmt.Println(futureRealValue)
 }
